1ba8ad1d4d95ca78d5e7f0fffdf3b6d5721f76bd
Pavel Ravvich <triodjangopiter@yandex.ru>
2021-01-27 02:49:33 +0300

    add Transfer entity

--- src/main/java/com/pravvich/demo/repository/TransferRepository.java
+++ src/main/java/com/pravvich/demo/repository/TransferRepository.java
@@ -1,9 +1,13 @@
 package com.pravvich.demo.repository;
 
 import com.pravvich.demo.model.Transfer;
+import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@JaversSpringDataAuditable
 public interface TransferRepository
         extends JpaRepository<Transfer, Long>, JpaSpecificationExecutor<Transfer> {
 }
