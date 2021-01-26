d37625fc778b1ce8d6025385c75c97281ea23df7
Pavel Ravvich <triodjangopiter@yandex.ru>
2021-01-27 02:07:39 +0300

    add Transfer entity

--- src/main/java/com/pravvich/demo/controller/AuditController.java
+++ src/main/java/com/pravvich/demo/controller/AuditController.java
@@ -2,6 +2,7 @@ package com.pravvich.demo.controller;
 
 import com.pravvich.demo.model.Account;
 import com.pravvich.demo.model.Company;
+import com.pravvich.demo.model.Transfer;
 import lombok.RequiredArgsConstructor;
 import org.javers.core.Javers;
 import org.javers.core.diff.Change;
@@ -20,14 +21,25 @@ public class AuditController {
 
     private final Javers javers;
 
-    @GetMapping("/company")
+    /**
+     * todo
+     * Register: create new transfer, edit transfer fields.
+     */
+    @GetMapping("/transfer")
     public ResponseEntity<String> getCompanyChanges() {
-        QueryBuilder jqlQuery = QueryBuilder.byClass(Company.class);
+        QueryBuilder jqlQuery = QueryBuilder.byClass(Transfer.class);
         List<Change> changes = javers.findChanges(jqlQuery.build());
         String resp = javers.getJsonConverter().toJson(changes);
         return ResponseEntity.ok(resp);
     }
 
+    /**
+     * todo
+     * Register: create new account & company, edit transfer & account fields.
+     *
+     * Support group by specific identifier based on javers.
+     * Support grouping different requests.
+     */
     @GetMapping("/account")
     public String getAccountChanges() {
         QueryBuilder jqlQuery = QueryBuilder.byClass(Account.class);
