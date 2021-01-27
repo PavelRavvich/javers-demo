7784c9fd162237ac577d52f0d93ec65137e7e707
Pavel Ravvich <triodjangopiter@yandex.ru>
2021-01-27 15:06:37 +0300

    Company request mapping

--- src/main/java/com/pravvich/demo/controller/AuditController.java
+++ src/main/java/com/pravvich/demo/controller/AuditController.java
@@ -3,8 +3,11 @@ package com.pravvich.demo.controller;
 import com.pravvich.demo.model.Account;
 import com.pravvich.demo.model.Company;
 import com.pravvich.demo.model.Transfer;
+import lombok.Builder;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.javers.core.Javers;
+import org.javers.core.commit.CommitId;
 import org.javers.core.diff.Change;
 import org.javers.repository.jql.QueryBuilder;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +15,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,7 +43,7 @@ public class AuditController {
     /**
      * todo
      * Register: create new account & company, edit transfer & account fields.
-     *
+     * <p>
      * Support group by specific identifier based on javers.
      * Support grouping different requests.
      */
@@ -44,6 +51,22 @@ public class AuditController {
     public String getAccountChanges() {
         QueryBuilder jqlQuery = QueryBuilder.byClass(Account.class);
         List<Change> changes = javers.findChanges(jqlQuery.build());
-        return javers.getJsonConverter().toJson(changes);
+        final Set<ChangeButch> butches = changes.stream().collect(Collectors.groupingBy(this::getId))
+                .values().stream()
+                .map(item ->
+                        ChangeButch.builder().changes(item).comment("My comment").build()
+                ).collect(Collectors.toSet());
+        return javers.getJsonConverter().toJson(butches);
+    }
+
+    private long getId(Change change) {
+        return change.getCommitMetadata().orElseThrow().getId().getMajorId();
+    }
+
+    @Builder
+    @Data
+    static class ChangeButch {
+        private String comment;
+        private List<Change> changes;
     }
 }
